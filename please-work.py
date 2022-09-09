edb798a51460fa6f96910bfe4b4709f4520db06e
Matthew Davey <github@project-2501.net>
2022-09-10 02:01:27 +1000

    Started on actual API

--- please-work.py
+++ please-work.py
@@ -8,7 +8,9 @@ class SimpleTcpClient:
     """
     Stolen from mboukhlouf/tinytcp
 
-    TODO: Shutdown flag
+    I think this is too simple.  I actually need to parse headers and manually group frames raw data into AGWPE
+    frames...  I'm not looking forward to that, it seems really complicated (how do I tell the difference between an
+    incomplete header, and corrupted data?  How long do I wait for the rest of the data?).
 
     multiple event handlers for incoming data by:
     def i_got_data(bytes):
@@ -47,7 +49,7 @@ class SimpleTcpClient:
 
             self._trigger_event(self.on_data, buffer)
 
-    def connect(self, host, port):
+    def connect(self, host: str, port: int):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._socket.connect((host, port))
 
@@ -60,7 +62,7 @@ class SimpleTcpClient:
         self._socket.close()
 
     def send(self, msg: bytes):
-        self._socket.send(msg)
+        self._socket.sendall(msg)
 
 
 class AgwpePacket:
@@ -166,52 +168,117 @@ class AgwpePacket:
         return p
 
 
-def handle_data(data: bytes):
-    print("Received Data")
-    print(AgwpePacket.decode(data))
+class AgwpeClient:
+    def __init__(self):
+        self.tcp_client = SimpleTcpClient()
+
+        self.on_version_info = []
+        self.on_unknown_packet = []
+
+    def connect(self, host: str, port: int):
+        self.tcp_client.on_data.append(self._handle_data)
+        self.tcp_client.connect(host, port)
+        self.tcp_client.start_read_loop()
+
+    def disconnect(self):
+        self.tcp_client.stop()
+
+    @staticmethod
+    def _trigger_event(what, *args):
+        for f in what:
+            f(*args)
+
+    def _handle_data(self, data: bytes):
+        try:
+            packet = AgwpePacket.decode(data)
+
+            if packet.kind == b'R':  # version response
+                (major, minor) = struct.unpack('HxxHxx', packet.data)  # this is wrong, or at least weird? 2005 127 ?
+                self._trigger_event(self.on_version_info, major, minor)
+            else:
+                print(packet)
+                self._trigger_event(self.on_unknown_packet, packet)
+
+        except Exception as e:
+            print("Unable to decode packet")
+            print(e)
+            print(repr(data))
+
+    def request_version_info(self):
+        packet = AgwpePacket()
+        packet.set_kind('R')
+        self.tcp_client.send(packet.encode())
+
+    def register_callsign(self, callsign):
+        packet = AgwpePacket()
+        packet.set_kind('X')
+        packet.set_call_from(callsign)
+        self.tcp_client.send(packet.encode())
+
+    def enable_monitoring(self):
+        packet = AgwpePacket()
+        packet.set_kind('m')
+        self.tcp_client.send(packet.encode())
+
+    def enable_raw_monitoring(self):
+        packet = AgwpePacket()
+        packet.set_kind('k')
+        self.tcp_client.send(packet.encode())
+
+    def send_unproto(self, call_from, call_to, data: bytes):
+        packet = AgwpePacket()
+        packet.set_kind('M')
+        packet.set_call_from(call_from)
+        packet.set_call_to(call_to)
+        packet.set_data(data)
+        self.tcp_client.send(packet.encode())
 
 
-print('Connecting...')
-client = SimpleTcpClient()
-client.on_data.append(handle_data)
-client.connect('localhost', 8000)
-client.start_read_loop()
+def print_version(major, minor):
+    print("Version", major, minor)
 
-print('Asking for Version Info...')
-p = AgwpePacket()
-p.set_kind('R')
-# p.set_data(b"\x00" * 4)  # Beats me why
-client.send(p.encode())
+
+# SoundModem_HS 1
+client1 = AgwpeClient()
+client1.connect('localhost', 8000)
+client1.on_version_info.append(print_version)
+client1.request_version_info()
+# client1.enable_raw_monitoring()
+
+# SoundModem_HS 2
+client2 = AgwpeClient()
+client2.connect('localhost', 9000)
+client2.on_version_info.append(print_version)
+client2.enable_monitoring()
+client2.enable_raw_monitoring()
+client2.request_version_info()
+# client2.register_callsign('VK3ARD')
+
 time.sleep(1)
 
-print('Asking for Port Capabilities...')
-p = AgwpePacket()
-p.set_kind('g')
-client.send(p.encode())
+# Oh no.  The 'T' frames from monitored are missing unprintable bytes.  They are sent, but stripped.
+# Spec says:  "containing the sent data in a fully transparent way (binary information,
+#              no delimiters, bit stuffing or escape codes)"
+#
+# They are missing from 'U' frames too...
+#
+# Thankfully, the raw data 'K' frames have the binary data... But I now need to parse AX.25 headers too ugh
+
+
+# But now I'm not getting U or K frames!?  WTF it was working before
+# OMG, don't transmit from client1 and client2 at the same time and expect things to work!?
+client1.send_unproto('CL1', 'PACPAL', b'Hello World 1 from 8000 \x00\x01\x02\x03\x04\x05\x06\x07 Where are my bytes!?')
+client1.send_unproto('CL1', 'PACPAL', b'Hello World 2 from 8000 \x00\x01\x02\x03\x04\x05\x06\x07 Where are my bytes!?')
+client1.send_unproto('CL1', 'PACPAL', b'Hello World 3 from 8000 \x00\x01\x02\x03\x04\x05\x06\x07 Where are my bytes!?')
+
 time.sleep(1)
 
-print('Asking for Port Information...')
-p = AgwpePacket()
-p.set_kind('G')
-client.send(p.encode())
+client2.send_unproto('CL2', 'PACPAL', b'Hello World 1 from 9000 \x00\x01\x02\x03\x04\x05\x06\x07 Where are my bytes!?')
+client2.send_unproto('CL2', 'PACPAL', b'Hello World 2 from 9000 \x00\x01\x02\x03\x04\x05\x06\x07 Where are my bytes!?')
+client2.send_unproto('CL2', 'PACPAL', b'Hello World 3 from 9000 \x00\x01\x02\x03\x04\x05\x06\x07 Where are my bytes!?')
+
 time.sleep(1)
 
-print('Turning on normal monitoring...')
-p = AgwpePacket()
-p.set_kind('m')
-client.send(p.encode())
+client1.disconnect()
+client2.disconnect()
 
-print('Sending UNPROTO AX.25 packet...')
-p = AgwpePacket()
-p.set_kind('M')
-p.set_call_from('VK3ARD-15')
-p.set_call_to('PACPAL')  # 6 characters!?  Is this a AX.25 limitation?
-p.set_data(b"0123456789abcdef"*4)
-client.send(p.encode())
-time.sleep(1)
-
-print('Waiting...')
-time.sleep(1)
-
-print('Stopping tcp connection...')
-client.stop()
